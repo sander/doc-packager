2e69234be9d6c9c5ea54b42e1b5340e3183366cc
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2022-07-10 15:44:45 +0200

    test: example directory generation

--- src/test/scala/nl/sanderdijkhuis/docpkg/StepDefinitions.scala
+++ src/test/scala/nl/sanderdijkhuis/docpkg/StepDefinitions.scala
@@ -1,10 +1,30 @@
 package nl.sanderdijkhuis.docpkg
 
 import io.cucumber.datatable.DataTable
-import io.cucumber.scala.{EN, PendingException, ScalaDsl}
+import io.cucumber.scala.{EN, PendingException, ScalaDsl, Scenario}
+import io.cucumber.scala.Implicits.*
+
+import java.io.File
+import java.nio.file.{Files, Path, Paths}
+import java.util.Comparator
 
 class StepDefinitions extends ScalaDsl with EN:
+  val directory: Path =
+    Files.createTempDirectory(Paths.get("target"), getClass.getName)
   var success: Boolean = false
+
+  Before {
+    directory.toFile.mkdirs()
+    ()
+  }
+
+  After {
+    Files
+      .walk(directory)
+      .sorted(Comparator.reverseOrder())
+      .map(_.toFile)
+      .forEach(_.delete)
+  }
 
   When("""I launch the application""") { () =>
     if (DocumentationPackager.run() == ()) success = true
@@ -13,7 +33,12 @@ class StepDefinitions extends ScalaDsl with EN:
     assert(success)
   }
   Given("""a directory with contents""") { (dataTable: DataTable) =>
-    throw PendingException()
+    for f <- dataTable.asScalaMaps
+        .map(_("File path").get)
+        .map(File(directory.toFile, _))
+    yield
+      f.getParentFile.mkdirs()
+      assert(f.createNewFile())
   }
   When("""I generate a local inventory""") { () =>
     throw PendingException()
