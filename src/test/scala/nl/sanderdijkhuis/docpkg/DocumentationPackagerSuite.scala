e8283c6214fef45cd4d6901b9fd66e865704a7cd
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2022-07-11 09:59:57 +0200

    style: DocumentationPackagerSuite brackets

--- src/test/scala/nl/sanderdijkhuis/docpkg/DocumentationPackagerSuite.scala
+++ src/test/scala/nl/sanderdijkhuis/docpkg/DocumentationPackagerSuite.scala
@@ -1,7 +1,6 @@
 package nl.sanderdijkhuis.docpkg
 
-class DocumentationPackagerSuite extends munit.FunSuite {
+class DocumentationPackagerSuite extends munit.FunSuite:
   test("run() returns") {
     assertEquals(DocumentationPackager.run(), ())
   }
-}
