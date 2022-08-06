4d983d4b8b0f167bbd787c84d0011bc68127372a
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2022-08-06 16:36:22 +0200

    feat: improve ContentManagement definitions

--- src/main/scala/nl/sanderdijkhuis/docpkg/ContentManagement.scala
+++ src/main/scala/nl/sanderdijkhuis/docpkg/ContentManagement.scala
@@ -1,18 +1,26 @@
 package nl.sanderdijkhuis.docpkg
 
+import java.nio.file.Path
+
 object ContentManagement:
 
-  private val nameRegex = raw"^[a-zA-Z0-9-]+\z".r
+  private val pageNameRegex = raw"^[a-zA-Z0-9-]+\z".r
+  private val attachmentNameRegex = raw"^[a-zA-Z0-9-.]+\z".r
 
   opaque type PageName = String
   object PageName:
-    def get(value: String): Option[PageName] = nameRegex.findFirstIn(value)
+    def get(value: String): Option[PageName] = pageNameRegex.findFirstIn(value)
+  extension (n: PageName) def value: String = n
 
   opaque type PagePath = List[PageName]
   object PagePath:
     def apply(name: PageName): PagePath = List(name)
+    def root: PagePath = List.empty
+    def appendTo(path: PagePath, name: PageName): PagePath = name :: path
+  extension (p: PagePath)
+    def toStringPath: String = "/" + p.reverse.mkString("/")
 
   opaque type AttachmentName = String
   object AttachmentName:
     def get(value: String): Option[AttachmentName] =
-      nameRegex.findFirstIn(value)
+      attachmentNameRegex.findFirstIn(value)
