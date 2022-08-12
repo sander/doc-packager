fe13c080e8722f5db8d2d349cfb550e5e083e133
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2022-08-12 19:59:05 +0200

    chore: dependency updates

--- src/acceptance.test.scala
+++ src/acceptance.test.scala
@@ -1,8 +1,9 @@
 //> using lib "com.github.sbt:junit-interface:0.13.3"
-//> using lib "io.cucumber::cucumber-scala::8.6.0"
-//> using lib "io.cucumber:cucumber-junit:7.5.0"
+//> using lib "io.cucumber::cucumber-scala:8.7.0"
+//> using lib "io.cucumber:cucumber-junit:7.6.0"
 //> using lib "junit:junit:4.13.2"
-//> using lib "org.scalameta::munit::0.7.29"
+//> using lib "org.scalameta::munit:0.7.29"
+// Do not update to 1.0.0-M6: https://github.com/scalameta/munit/issues/540
 //> using testFramework "com.novocode.junit.JUnitFramework"
 //> using resourceDirs "../features"
 
