2e5f485234e04a1c00a6cebdb9b5e29e07c6d14a
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2022-08-27 13:52:24 +0200

    refactor: extract attribute function

--- src/formatting.test.scala
+++ src/formatting.test.scala
@@ -6,11 +6,14 @@ import scala.xml.*
 
 class FormattingSuite extends munit.FunSuite:
 
+  def attribute(elem: Elem, prefix: String, name: String): Option[String] =
+    elem.attributes.collectFirst {
+      case PrefixedAttribute(prefix, name, value, _) => value.text
+    }
+
   test("renders a directory to a list of its children") {
     val page = directoryPage()
     assertEquals(page.value.prefix, "ac")
     assertEquals(page.value.label, "structured-macro")
-    assertEquals(page.value.attributes.collectFirst {
-      case PrefixedAttribute("ac", "name", value, _) => value.text
-    }, Some("children"))
+    assertEquals(attribute(page.value, "ac", "name"), Some("children"))
   }
