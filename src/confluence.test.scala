87d115158c900c80873fdf77b1a84fb2035303d7
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2022-08-27 11:43:31 +0200

    test: appendTo, createProperty

--- src/confluence.test.scala
+++ src/confluence.test.scala
@@ -1,7 +1,5 @@
 //> using testFramework "munit.Framework"
 //> using lib "com.softwaremill.sttp.client3::core:3.7.4"
-//> using lib "com.softwaremill.sttp.client3::scribe-backend:3.7.4"
-//> using lib "com.outr::scribe:3.10.2"
 
 package docpkg
 
@@ -9,7 +7,6 @@ import docpkg.confluence.*
 import docpkg.content.*
 
 import sttp.client3.*
-import sttp.client3.logging.scribe.ScribeLoggingBackend
 
 import java.util.UUID
 import java.nio.charset.StandardCharsets
@@ -19,6 +16,12 @@ class ConfluenceSuite extends munit.FunSuite:
 
   val integration = munit.Tag("integration")
 
+  val tokenName = "ATLASSIAN_API_TOKEN"
+  val token = sys.env
+    .get(tokenName)
+    .flatMap(Token.from)
+    .getOrElse(sys.error(s"Set the $tokenName environment variable"))
+
   given Access = Access(
     sys.env.get("ATLASSIAN_API_TOKEN").flatMap(Token.from).get,
     Domain.from("sanderd.atlassian.net").get,
@@ -27,6 +30,18 @@ class ConfluenceSuite extends munit.FunSuite:
 
   val backend = HttpClientSyncBackend()
   val space = SpaceKey.parse("DOCPKGIT").get
+  val propertyKey = PropertyKey.parse("test-key").get
+
+  val page = FunFixture[Id](
+    setup = { test =>
+      val uuid = UUID.randomUUID().toString()
+      val title = Title.parse(s"${test.name} ($uuid)").get
+      val body = Body.parse("").get
+
+      createPage(space, title, body).send(backend).body
+    },
+    teardown = { id => deletePage(id).send(backend) }
+  )
 
   test("gets a space property".tag(integration).ignore) {
     val property = PropertyKey.parse("non-existent-test-property").get
@@ -51,18 +66,9 @@ class ConfluenceSuite extends munit.FunSuite:
     assert(response.contains(value))
   }
 
-  test("creates and deletes pages".tag(integration)) {
-    val title = Title.parse(UUID.randomUUID().toString()).get
-    val body = Body.parse("").get
+  page.test("creates and deletes pages".tag(integration)) { id => () }
 
-    val id = createPage(space, title, body).send(backend).body
-
-    deletePage(id).send(backend)
-  }
-
-  test("uploads and gets attachments".tag(integration)) {
-    val title = Title.parse(UUID.randomUUID().toString()).get
-    val body = Body.parse("").get
+  page.test("uploads and gets attachments".tag(integration)) { contentId =>
     val attachmentName = AttachmentName.get("attachment.txt").get
     val attachmentData = "Hello, World!\n".getBytes(StandardCharsets.UTF_8)
     val path = Files.createTempFile(Paths.get("/tmp"), "attachment", ".txt")
@@ -70,36 +76,38 @@ class ConfluenceSuite extends munit.FunSuite:
     val comment = Comment.from("test-comment").get
 
     Files.write(path, attachmentData)
-    val contentId = createPage(space, title, body).send(backend).body
     val attachmentId = attach(contentId, attachment, comment).send(backend).body
     Files.delete(path)
     val out = getAttachment(contentId, attachmentId).send(backend).body
 
     assertEquals(out.toSeq, attachmentData.toSeq)
-
-    deletePage(contentId).send(backend)
   }
 
-  test("updates pages".tag(integration)) {
-    val title = Title.parse(UUID.randomUUID().toString()).get
-    val body1 = Body.parse("one").get
+  page.test("updates pages".tag(integration)) { id =>
     val body2 = Body.parse("two").get
 
-    val id = createPage(space, title, body1).send(backend).body
     val content1 = getContentForUpdate(id).send(backend).body
-    updatePage(id, content1.version.increment, title, body2).send(backend)
+    updatePage(id, content1.version.increment, content1.title, body2)
+      .send(backend)
     val content2 = getContentForUpdate(id).send(backend).body
 
     assertEquals(content2.body, body2)
-
-    deletePage(id).send(backend)
   }
 
-  test("lists pages without throwing an error".tag(integration)) {
-    // TODO: test behavior
+  FunFixture
+    .map2(page, page)
+    .test("moves pages in the navigation menu without error".tag(integration)) {
+      (id1, id2) =>
+        appendTo(id1, id2).send(backend)
+    }
 
-    val key = PropertyKey.parse("test-key").get
-    val response = getPagesWithProperty(key).send(backend).body
+  page.test("creates and finds page properties".tag(integration)) { id =>
+    val uuid = UUID.randomUUID().toString()
+    val propertyKey = PropertyKey.parse(s"test-$uuid").get
+
+    createProperty(id, propertyKey, List("testing")).send(backend)
+
+    val response = getPagesWithProperty(propertyKey).send(backend).body
+
+    assert(response.inventory.values.exists(_.id == id))
   }
-
-  // TODO: test appendTo, createProperty
