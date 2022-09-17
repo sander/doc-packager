c8938a5a6c92a224d80a2d9a1cafb1f5409bde84
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2022-09-17 23:04:07 +0200

    feat: detect git

--- src/main/java/docpkg/Main.java
+++ src/main/java/docpkg/Main.java
@@ -1,14 +1,78 @@
 package docpkg;
 
-import static java.util.Objects.requireNonNullElse;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Main {
 
-  private static final String name = "Documentation Packager";
+  public static final SemanticVersion minimumContentTrackerVersion =
+    new SemanticVersion("git", 2, 37, 0);
+
+  public static boolean checkContentTrackerCompatibility() {
+    return getContentTrackerVersion()
+      .flatMap(SemanticVersion::from)
+      .map(minimumContentTrackerVersion::isMetBy)
+      .orElse(false);
+  }
 
   public static void main(String[] args) {
-    var p = Main.class.getPackage();
-    var v = requireNonNullElse(p.getImplementationVersion(), "(Not packaged)");
-    System.out.printf("%s %s\n", name, v);
+    System.out.printf("%s %s\n", name, getVersion().orElse(defaultVersion));
+  }
+
+  private static final String defaultVersion = "(Not packaged)";
+  private static final String name = "Documentation Packager";
+
+  private static Optional<String> getContentTrackerVersion() {
+    try {
+      var process =
+        new ProcessBuilder(minimumContentTrackerVersion.name, "version")
+          .start();
+      var bytes = process.getInputStream().readAllBytes();
+      return Optional.of(new String(bytes).trim());
+    } catch (IOException e) {
+      return Optional.empty();
+    }
+  }
+
+  private static Optional<String> getVersion() {
+    return Optional
+      .ofNullable(Main.class.getPackage().getImplementationVersion());
+  }
+
+  private record SemanticVersion(String name, int major, int minor, int patch) {
+
+    static Optional<SemanticVersion> from(String s) {
+      var pattern =
+        Pattern.compile("([^ ]+) version (\\d+)\\.(\\d+)\\.(\\d+) \\(.*\\)");
+      return Optional.of(s)
+        .map(pattern::matcher)
+        .filter(Matcher::matches)
+        .map(m -> {
+          var components = Stream.of(2, 3, 4)
+            .map(m::group)
+            .map(Integer::parseUnsignedInt)
+            .toList();
+          return new SemanticVersion(
+            m.group(1),
+            components.get(0),
+            components.get(1),
+            components.get(2)
+          );
+        });
+    }
+
+    boolean isMetBy(SemanticVersion other) {
+      return other.isCompatibleWith(this);
+    }
+
+    boolean isCompatibleWith(SemanticVersion requirement) {
+      return name.equals(requirement.name) &&
+        major == requirement.major
+        && minor >= requirement.minor
+        && (minor > requirement.minor || patch >= requirement.patch);
+    }
   }
 }
--- src/test/java/docpkg/MainTests.java
+++ src/test/java/docpkg/MainTests.java
@@ -1,11 +1,20 @@
 package docpkg;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTests {
 
   @Test
   void runsWithoutErrors() {
     Main.main(new String[]{});
+  }
+
+  @Test
+  @Tag("integration")
+  void detectsContentTrackerAssumingGitIsInstalled() {
+    assertTrue(Main.checkContentTrackerCompatibility());
   }
 }
