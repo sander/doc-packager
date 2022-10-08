852f52531c5bb1c3fd1abd1a3f117b4a4eb70af1
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2022-10-09 00:44:22 +0200

    test: set up example project for integration tests

--- src/test/java/docpkg/DocumentationPackagingTests.java
+++ src/test/java/docpkg/DocumentationPackagingTests.java
@@ -3,6 +3,8 @@ package docpkg;
 import docpkg.DocumentationPackaging.FileDescription;
 import docpkg.DocumentationPackaging.PackageName;
 import docpkg.DocumentationPackaging.Service;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -10,6 +12,7 @@ import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -22,6 +25,32 @@ public class DocumentationPackagingTests {
 
   final PackageName name = new PackageName("main");
   final ContentTracking.Service content = new ContentTracking.GitService();
+
+  static final Path testDirectory = Path.of("target/test-packaging");
+  static final Path origin = testDirectory.resolve("origin");
+  static final Path clone = testDirectory.resolve("clone");
+
+  @BeforeEach
+  void setUp() {
+    var service = new ContentTracking.GitService();
+    var resource = Objects.requireNonNull(
+        getClass().getClassLoader().getResource("docpkg/example"));
+    var source = Path.of(resource.getPath());
+
+    FileOperations.removeRecursively(testDirectory);
+    FileOperations.copyRecursively(source, origin);
+    service.initialize(origin);
+    service.addCurrentWorktree(origin);
+    var maybeId = service.commit(origin,
+        new ContentTracking.CommitMessage("feat: initial commit"));
+    assert maybeId.isPresent();
+    service.clone(origin, clone);
+  }
+
+  @AfterEach
+  void tearDown() {
+    FileOperations.removeRecursively(testDirectory);
+  }
 
   @Test
   @Tag("integration")
