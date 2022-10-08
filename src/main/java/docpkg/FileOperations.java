6cb9dd46a865ba949df47e1ea9a597b7f12d92b1
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2022-10-09 00:28:47 +0200

    feat: add copyRecursively

--- src/main/java/docpkg/FileOperations.java
+++ src/main/java/docpkg/FileOperations.java
@@ -1,8 +1,8 @@
 package docpkg;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Comparator;
 
 public class FileOperations {
@@ -20,6 +20,42 @@ public class FileOperations {
       } catch (IOException e) {
         throw new RuntimeException("Could not walk", e);
       }
+    }
+  }
+
+  static void copyRecursively(Path from, Path to) {
+    try {
+      Files.walkFileTree(from, new CopyVisitor(from, to));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static class CopyVisitor extends SimpleFileVisitor<Path> {
+    final Path from;
+    final Path to;
+
+    CopyVisitor(Path from, Path to) {
+      this.from = from;
+      this.to = to;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(
+        Path dir, BasicFileAttributes attrs) throws IOException {
+      Files.createDirectories(transform(dir));
+      return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(
+        Path file, BasicFileAttributes attrs) throws IOException {
+      Files.copy(file, transform(file), StandardCopyOption.COPY_ATTRIBUTES);
+      return FileVisitResult.CONTINUE;
+    }
+
+    private Path transform(Path path) {
+      return to.resolve(from.relativize(path));
     }
   }
 }
