cdfd2ceee693feb09063b17b27a2813e59dae3b9
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2022-10-09 12:34:03 +0200

    feat: parse sexps

--- src/main/java/docpkg/SymbolicExpressions.java
+++ src/main/java/docpkg/SymbolicExpressions.java
@@ -1,15 +1,76 @@
 package docpkg;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StreamTokenizer;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
 public class SymbolicExpressions {
 
-  static Optional<Expression> read(String input) {
-    if (input.equals("()")) {
-      return Optional.of(Expression.SymbolicList.empty());
+  static Optional<Expression> read(Reader reader) {
+    return parse(tokenize(reader));
+  }
+
+  static List<Token> tokenize(Reader reader) {
+    var tokenizer = new StreamTokenizer(new BufferedReader(reader));
+    tokenizer.resetSyntax();
+    tokenizer.whitespaceChars(0, ' ');
+    tokenizer.wordChars(' ' + 1, 255);
+    tokenizer.ordinaryChar('(');
+    tokenizer.ordinaryChar(')');
+    tokenizer.ordinaryChar('\'');
+    tokenizer.commentChar(';');
+    tokenizer.quoteChar('"');
+
+    var result = new LinkedList<Token>();
+    while (true) {
+      try {
+        tokenizer.nextToken();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+      if (tokenizer.ttype == StreamTokenizer.TT_EOF) {
+        return result;
+      } else {
+        result.add(new Token(tokenizer.ttype, tokenizer.sval, tokenizer.lineno()));
+      }
     }
-    return Optional.empty();
+  }
+
+  static Optional<Expression> parse(List<Token> input) {
+    return parsePart(input).filter(result -> result.rest().isEmpty()).map(ParseResult::expression);
+  }
+
+  private static Optional<ParseResult> parsePart(List<Token> input) {
+    var token = input.get(0);
+    var rest = input.subList(1, input.size());
+    return switch (token.type) {
+      case '(' -> parseList(rest);
+      case '"' -> Optional.of(new ParseResult(new Expression.StringAtom(token.text), rest));
+      case StreamTokenizer.TT_WORD -> Optional.of(new ParseResult(new Expression.Atom(token.text), rest));
+      default -> throw new RuntimeException(String.format("Unknown token type %d", token.type));
+    };
+  }
+
+  private static Optional<ParseResult> parseList(List<Token> input) {
+    var result = new LinkedList<Expression>();
+    var rest = input;
+    if (rest.isEmpty()) {
+      return Optional.empty();
+    }
+    while (rest.get(0).type != ')') {
+      var element = parsePart(rest);
+      if (element.isEmpty()) return Optional.empty();
+      result.add(element.get().expression());
+      rest = element.get().rest();
+      if (rest.isEmpty()) {
+        return Optional.empty();
+      }
+    }
+    return Optional.of(new ParseResult(new Expression.SymbolicList(result), rest.subList(1, rest.size())));
   }
 
   sealed interface Expression {
@@ -20,5 +81,13 @@ public class SymbolicExpressions {
         return new SymbolicList(List.of());
       }
     }
+
+    record StringAtom(String value) implements Expression {}
+
+    record Atom(String value) implements Expression {}
   }
+
+  record ParseResult(Expression expression, List<Token> rest) {}
+
+  record Token(int type, String text, int line) {}
 }
--- src/test/java/docpkg/SymbolicExpressionsTest.java
+++ src/test/java/docpkg/SymbolicExpressionsTest.java
@@ -1,8 +1,12 @@
 package docpkg;
 
+import docpkg.SymbolicExpressions.Expression.Atom;
+import docpkg.SymbolicExpressions.Expression.StringAtom;
 import docpkg.SymbolicExpressions.Expression.SymbolicList;
 import org.junit.jupiter.api.Test;
 
+import java.io.StringReader;
+import java.util.List;
 import java.util.Optional;
 
 import static docpkg.SymbolicExpressions.read;
@@ -13,12 +17,25 @@ public class SymbolicExpressionsTest {
   @Test
   void testInvalidInput() {
     var input = "(";
-    assertEquals(Optional.empty(), read(input));
+    assertEquals(Optional.empty(), read(new StringReader(input)));
   }
 
   @Test
   void testEmptyList() {
     var input = "()";
-    assertEquals(Optional.of(SymbolicList.empty()), read(input));
+    assertEquals(Optional.of(SymbolicList.empty()), read(new StringReader(input)));
+  }
+
+  @Test
+  void testParse() {
+    // Example input from https://rosettacode.org/wiki/S-expressions
+    var input = "((data \"quoted data\" 123 4.5)\n" + " (data (!@# (4.5) \"(more\" \"data)\")))";
+
+    var result = SymbolicExpressions.read(new StringReader(input));
+
+    var data1 = new SymbolicList(List.of(new Atom("data"), new StringAtom("quoted data"), new Atom("123"), new Atom("4.5")));
+    var data2 = new SymbolicList(List.of(new Atom("data"), new SymbolicList(List.of(new Atom("!@#"), new SymbolicList(List.of(new Atom("4.5"))), new StringAtom("(more"), new StringAtom("data)")))));
+
+    assertEquals(Optional.of(new SymbolicList(List.of(data1, data2))), result);
   }
 }
