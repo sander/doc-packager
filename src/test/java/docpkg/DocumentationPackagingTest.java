56acb15dfa1911ac109c6d86273b3fa33d4f32b9
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2022-10-09 09:34:40 +0200

    refactor: improve DocumentationPackagingTest structure

--- src/test/java/docpkg/DocumentationPackagingTest.java
+++ src/test/java/docpkg/DocumentationPackagingTest.java
@@ -1,5 +1,6 @@
 package docpkg;
 
+import docpkg.ContentTracking.CommitMessage;
 import docpkg.DocumentationPackaging.FileDescription;
 import docpkg.DocumentationPackaging.PackageName;
 import docpkg.DocumentationPackaging.Service;
@@ -7,44 +8,37 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DocumentationPackagingTest {
 
-  private static final Logger logger =
-      LoggerFactory.getLogger(DocumentationPackagingTest.class);
-
   final PackageName name = new PackageName("main");
   final ContentTracking.Service content = new ContentTracking.GitService();
 
-  static final Path testDirectory = Path.of("target/test-packaging");
-  static final Path origin = testDirectory.resolve("origin");
-  static final Path clone = testDirectory.resolve("clone");
+  final Path testDirectory = Path.of("target/test-packaging");
+  final Path origin = testDirectory.resolve("origin");
+  final Path clone = testDirectory.resolve("clone");
+
+  final CommitMessage initialCommitMessage =
+      new CommitMessage("feat: initial commit");
 
   @BeforeEach
   void setUp() {
-    var service = new ContentTracking.GitService();
-    var resource = Objects.requireNonNull(
-        getClass().getClassLoader().getResource("docpkg/example"));
-    var source = Path.of(resource.getPath());
+    var source = getResource("docpkg/example");
 
     FileOperations.removeRecursively(testDirectory);
     FileOperations.copyRecursively(source, origin);
-    service.initialize(origin);
-    service.addCurrentWorktree(origin);
-    var maybeId = service.commit(origin,
-        new ContentTracking.CommitMessage("feat: initial commit"));
-    assert maybeId.isPresent();
-    service.clone(origin, clone);
+    content.initialize(origin);
+    content.addCurrentWorktree(origin);
+    assert content.commit(origin, initialCommitMessage).isPresent();
+    content.clone(origin, clone);
   }
 
   @AfterEach
@@ -62,27 +56,31 @@ public class DocumentationPackagingTest {
   void testPackageNameValidation() {
     Stream.of("a", "a/b", "a-b").forEach(PackageName::new);
     assertThrows(NullPointerException.class, () -> new PackageName(null));
-    Stream.of("", "A", "a".repeat(256), "-", "a:b").forEach(s ->
-        assertThrows(IllegalArgumentException.class,
-            () -> new PackageName(s)));
-  }
 
-  private FileDescription getResourceFileDescription(String s) {
-    var url = getClass().getClassLoader().getResource(s);
-    assert url != null;
-    var workingDirectory = Paths.get("").toAbsolutePath();
-    var absolutePath = Path.of(url.getPath());
-    var relativePath = workingDirectory.relativize(absolutePath);
-    return new DocumentationPackaging.FileDescription(relativePath);
+    var illegal = Stream.of("", "A", "a".repeat(256), "-", "a:b");
+    illegal.forEach(s ->
+        assertThrows(IllegalArgumentException.class, () -> new PackageName(s)));
   }
 
   @Test
   @Tag("integration")
   void testPublishing() {
     Service service = new DocumentationPackaging.Live(content, name);
-    service.publish(Set.of(
+    var descriptions = Set.of(
         getResourceFileDescription("docpkg/example/document.md"),
         getResourceFileDescription("docpkg/example/document-2.md")
-    ));
+    );
+    service.publish(descriptions);
+  }
+
+  private Path getResource(String name) {
+    var url = requireNonNull(getClass().getClassLoader().getResource(name));
+    return Path.of(url.getPath());
+  }
+
+  private FileDescription getResourceFileDescription(String s) {
+    var workingDirectory = Paths.get("").toAbsolutePath();
+    var relativePath = workingDirectory.relativize(getResource(s));
+    return new DocumentationPackaging.FileDescription(relativePath);
   }
 }
